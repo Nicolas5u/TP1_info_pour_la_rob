499dbc5471b6668ab4ba7be330e27c91f5fd423b
Nicolas <nicolasdurand34080@gmail.com>
2025-09-23 15:10:29 +0200

    fonction piocher et création du deck apres mix des deux versions

--- TP2/TP1_exo_Jeu_de_Carte.py
+++ TP2/TP1_exo_Jeu_de_Carte.py
@@ -3,9 +3,35 @@ import random
 
 
 class Cartes(ABC):
+
     def appliquer(self, joueur):
         pass
+        
+    def __init__(self, liste_carte):
+        self.liste_carte = []
+    
+    def initialise_et_melange_liste(liste_carte):
 
+    for i in range(30):
+        liste_carte.append(CarteNormale)
+
+    for i in range(6):
+        liste_carte.append(CarteBonus)
+
+    for i in range(5):
+        liste_carte.append(CarteMalus)
+
+    for i in range(15):
+        liste_carte.append(CarteChance)
+    
+    random.shuffle(liste_carte) # Mélanger l'ordre des cartes
+
+    return liste_carte
+    
+def piocher(liste_carte):
+    carte_sortie = liste_carte.pop(0)
+    
+    return carte_sortie
 
 
 class CarteNormale(Cartes):
@@ -18,23 +44,29 @@ class CarteNormale(Cartes):
         return f'Carte normale + {points} points'
 
 
-class Cartebonus(Cartes):
-
+class CarteBonus(Cartes): #regle
+    
+    def __init__(self):
+    
     def valeur(self, joueur):
         joueur.ajouter_score(joueur.score)
         return f'Carte Bonus+ {points} points'
 
 
 class CarteMalus(Cartes):
-cd
+    
+    def __init__(self):
+    
     def valeur(self, joueur):
-        points=-5
+        points =-5
         joueur.ajouter_score(points)
         return f'Carte malus {points} points'
 
 
 class CarteChance(Cartes):
-
+    
+    def __init__(self):
+    
     def valeur(self, joueur):
         points=random.randint(-5, 15)
         joueur.ajouter_score(points)
